7e08b5ce32a9cffaca722f4fca4666c0fbad86c9
RPiAwesomeness <keyboardmailtesting@gmail.com>
2019-10-07 12:20:46 -0400

    Added executable functionality to counting/insertion sort

--- SortingAlgos/python/counting_sort.py
+++ SortingAlgos/python/counting_sort.py
@@ -1,4 +1,4 @@
-def sort(array: list) -> list:
+def counting_sort(array: list) -> list:
     """
     Implementation of the linear O(n) Counting Sort algorithm
 
@@ -35,3 +35,21 @@ def sort(array: list) -> list:
         count[array[i]] -= 1
 
     return sorted_arr
+
+
+if __name__ == "__main__":
+    import random
+
+    NUM_VALUES = 10
+    MAX_VALUE = 100
+
+    # Uncomment to generate consistent random values between each run
+    # random.seed(0xd34db33f)
+
+    # Generate list of NUM_VALUES random values in range [0, MAX_VALUE]
+    unsorted = [random.randint(0, NUM_VALUES) for _ in range(0, MAX_VALUE + 1)]
+
+    # Perform sort, asserting result matches Python's default sorting functionality
+    sorted_arr = counting_sort(unsorted)
+    print("Sorted successfully!" if sorted_arr == sorted(unsorted) else "Did not sort!")
+        
--- SortingAlgos/python/insertion_sort.py
+++ SortingAlgos/python/insertion_sort.py
@@ -1,4 +1,4 @@
-def sort(source_array: list) -> list:
+def insertion_sort(source_array: list) -> list:
     """
     Implementation of the polynomial O(n^2) Insertion Sort algorithm
 
@@ -19,10 +19,28 @@ def sort(source_array: list) -> list:
         j = i - 1
 
         # Iterate until current position >= key
-        while j > 0 and array[j] > key:
+        while j >= 0 and array[j] > key:
             array[j + 1] = array[j]
             j -= 1
 
         array[j + 1] = key
 
     return array
+
+
+if __name__ == "__main__":
+    import random
+
+    NUM_VALUES = 10
+    MAX_VALUE = 100
+
+    # Uncomment to generate consistent random values between each run
+    # random.seed(0xd34db33f)
+
+    # Generate list of NUM_VALUES random values in range [0, MAX_VALUE]
+    unsorted = [random.randint(0, NUM_VALUES) for _ in range(0, MAX_VALUE + 1)]
+
+    # Perform sort, asserting result matches Python's default sorting functionality
+    sorted_arr = insertion_sort(unsorted)
+    print("Sorted successfully!" if sorted_arr ==
+          sorted(unsorted) else "Did not sort!")
