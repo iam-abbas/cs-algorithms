e861f70c76af3cc93d0d963cc0e3e1872c0d59d9
Saeed <SaeedEY@users.noreply.github.com>
2019-10-07 11:15:09 +0330

    Bubble Sort
    Bubble Sort Python Algorithm

---
+++ Searching and Sorting/Bubble Sort/bubble sort.py
@@ -0,0 +1,13 @@
+# Source : https://www.geeksforgeeks.org/python-program-for-bubble-sort/
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubbleSort(arr)
+print ("Sorted array is:")
+for i in range(len(arr)):
+    print ("%d" %arr[i]), 
