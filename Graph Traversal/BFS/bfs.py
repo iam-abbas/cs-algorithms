6e666904e3fdea8fa695eab963f3db2c2d6f11af
itayfry <itayfry@gmail.com>
2019-10-12 11:28:53 +0300

    BFS implementation in python fixes

--- Graph Traversal/BFS/bfs.py
+++ Graph Traversal/BFS/bfs.py
@@ -1,13 +1,14 @@
 import collections
 def bfs(graph, root): 
-    visited, queue = set(), collections.deque([root])
-    visited.add(root)
+    visited, queue = [], collections.deque([root])
+    visited.append(root)
     while queue: 
         vertex = queue.popleft()
         for neighbour in graph[vertex]: 
             if neighbour not in visited: 
-                visited.add(neighbour) 
-                queue.append(neighbour) 
+                visited.append(neighbour) 
+                queue.append(neighbour)
+    return visited
 if __name__ == '__main__':
-    graph = {0: [1, 2], 1: [2], 2: [3], 3: [1,2]} 
-    breadth_first_search(graph, 0)
+    graph = {0: [1,3], 1: [2], 2: [3], 3: [1,2]} 
+    print bfs(graph, 0)
