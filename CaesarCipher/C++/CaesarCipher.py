a76bd221895a83c108192f72f2fb5bbe08da50e9
rock8forever <39243599+rock8forever@users.noreply.github.com>
2019-10-13 14:57:50 +0500

    Delete CaesarCipher.py

--- CaesarCipher/C++/CaesarCipher.py
+++
@@ -1,7 +0,0 @@
-letters = input()
-rot = int(input())
-for i in range(len(letters)):
-    print(letters[(i + rot) % len(letters)], end='')
-print('\n', letters, sep='')
-for i in range(len(letters)):
-    print(letters[(i - rot) % len(letters)], end='')
