05de99574367227fd5bd140a770e43cfaf3bdfe6
Naman1233 <45735998+Naman1233@users.noreply.github.com>
2019-10-15 14:41:15 +0530

    tiling-problem.cpp
    Problem Statement: Count number of ways to tile a floor of size n x m using 1 x m tiles, using DP(Bottom-up manner).

---
+++ tiling-problem.cpp
@@ -0,0 +1,31 @@
+//Refrence: https://www.geeksforgeeks.org/count-number-ways-tile-floor-size-n-x-m-using-1-x-m-size-tiles/
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int countWays(int n, int m) 
+{ 
+
+	int count[n + 1]; 
+	count[0] = 0; 
+
+	for (int i = 1; i <= n; i++) {  
+		if (i > m) 
+			count[i] = count[i - 1] + count[i - m]; 
+ 
+		else if (i < m) 
+			count[i] = 1; 
+
+		else
+			count[i] = 2; 
+	} 
+
+	return count[n]; 
+} 
+
+int main() 
+{ 
+	int n = 7, m = 4; 
+	cout << "Number of ways = "
+		<< countWays(n, m); 
+	return 0; 
+} 
