7ae98968c5c8568475540861814f1032817ae2bc
tryingveryhard <51233551+tryingveryhard@users.noreply.github.com>
2019-10-15 19:42:06 +0530

    Improved Readability of code.

--- C++/decimal_to_octal.cpp
+++ C++/decimal_to_octal.cpp
@@ -4,17 +4,24 @@ using namespace std;
 
 int main()
 {
-    int n; cin>>n;
-    int i=0;
-    int rem;
-    int octal=0;
+    int n; 
+    //Input number
+    cout<<"Enter number in decimal: "
+    cin >> n;
+    int i=0, rem, octal=0;
+    
+    //While number is greater than zero
     while(n>0)
     {
-        rem=n%8;
-        octal=octal+rem*pow(10,i);
-        n=n/8;
+        //% calculate the remainder. In this case after dividing by 8
+        rem = n % 8;
+        
+        octal = octal + rem * pow(10,i);
+        n = n / 8;
         i++;
     }
-    cout<<octal;
+    
+    //Number in octal
+    cout << "Number in octal is: " << octal;
 
 }
