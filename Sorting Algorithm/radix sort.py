c62541a3f4bae511658dacc4f10faa489a01e205
likeme <33179335+likeme@users.noreply.github.com>
2019-10-17 13:51:06 +0530

    Radix sort

---
+++ Sorting Algorithm/radix sort.py
@@ -0,0 +1,28 @@
+def countingSort(array, place):
+    size = len(array)
+    output = [0] * size
+    count = [0] * 10
+    for i in range(0, size):
+        index = array[i] // place
+        count[index % 10] += 1
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    i = size - 1
+    while i >= 0:
+        index = array[i] // place
+        output[count[index % 10] - 1] = array[i]
+        count[index % 10] -= 1
+        i -= 1
+    for i in range(0, size):
+        array[i] = output[i]
+def radixSort(array):
+    max_element = max(array)
+    place = 1
+    while max_element // place > 0:
+        countingSort(array, place)
+        place *= 10
+
+alist = input('Enter the list of (nonnegative) numbers: ').split()
+data = [int(x) for x in alist]
+radixSort(data)
+print(data)
