da7d3c1fc45c59768c74fb4b13878d088594fb58
Saulo Santiago <saulo8732@hotmail.com>
2019-10-21 19:58:47 -0300

    Caesar Cipher in JS

---
+++ CaesarCipher/JavaScript/CaesarCipher.js
@@ -0,0 +1,42 @@
+var str = "abc test string";
+var amount = 1;
+
+console.log(cipher(str, amount));
+
+function cipher(str, amount) {// Wrap the amount
+	if (amount < 0)
+		return caesarShift(str, amount + 26);
+
+	// Make an output variable
+	var output = '';
+
+	// Go through each character
+	for (var i = 0; i < str.length; i ++) {
+
+		// Get the character we'll be appending
+		var c = str[i];
+
+		// If it's a letter...
+		if (c.match(/[a-z]/i)) {
+
+			// Get its code
+			var code = str.charCodeAt(i);
+
+			// Uppercase letters
+			if ((code >= 65) && (code <= 90))
+				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+
+			// Lowercase letters
+			else if ((code >= 97) && (code <= 122))
+				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+
+		}
+
+		// Append
+		output += c;
+
+	}
+
+	// All done!
+	return output;
+}
