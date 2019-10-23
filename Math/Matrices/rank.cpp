09c65eb896d13e22a3221104875284fb25f5f492
mysterion <lunu1997@gmail.com>
2019-10-23 17:56:46 +0530

    added matrix rank algorithm

--- Math/Matrices/rank.cpp
+++ Math/Matrices/rank.cpp
@@ -5,42 +5,61 @@ using namespace std;
 
 const double EPS = 1E-9;
 
-int compute_rank(vector<vector<double>> A) {
-    int n = A.size();
-    int m = A[0].size();
+const int INF = 2; // it doesn't actually have to be infinity or a big number
 
-    int rank = 0;
-    vector<bool> row_selected(n, false);
-    for (int i = 0; i < m; ++i) {
-        int j;
-        for (j = 0; j < n; ++j) {
-            if (!row_selected[j] && abs(A[j][i]) > EPS)
-                break;
-        }
+int gauss (vector < vector<double> > a, vector<double> & ans) {
+    int n = (int) a.size();
+    int m = (int) a[0].size() - 1;
 
-        if (j != n) {
-            ++rank;
-            row_selected[j] = true;
-            for (int p = i + 1; p < m; ++p)
-                A[j][p] /= A[j][i];
-            for (int k = 0; k < n; ++k) {
-                if (k != j && abs(A[k][i]) > EPS) {
-                    for (int p = i + 1; p < m; ++p)
-                        A[k][p] -= A[j][p] * A[k][i];
-                }
+    vector<int> where (m, -1);
+    for (int col=0, row=0; col<m && row<n; ++col) {
+        int sel = row;
+        for (int i=row; i<n; ++i)
+            if (abs (a[i][col]) > abs (a[sel][col]))
+                sel = i;
+        if (abs (a[sel][col]) < EPS)
+            continue;
+        for (int i=col; i<=m; ++i)
+            swap (a[sel][i], a[row][i]);
+        where[col] = row;
+
+        for (int i=0; i<n; ++i)
+            if (i != row) {
+                double c = a[i][col] / a[row][col];
+                for (int j=col; j<=m; ++j)
+                    a[i][j] -= a[row][j] * c;
             }
-        }
+        ++row;
     }
-    return rank;
+
+    ans.assign (m, 0);
+    for (int i=0; i<m; ++i)
+        if (where[i] != -1)
+            ans[i] = a[where[i]][m] / a[where[i]][i];
+    for (int i=0; i<n; ++i) {
+        double sum = 0;
+        for (int j=0; j<m; ++j)
+            sum += ans[j] * a[i][j];
+        if (abs (sum - a[i][m]) > EPS)
+            return 0;
+    }
+
+    for (int i=0; i<m; ++i)
+        if (where[i] == -1)
+            return INF;
+    return 1;
 }
 
 int main() {
-  cout << compute_rank(
+  vector<int> ans;
+  cout << gauss(
   {
     {-5, -6, 4},
     {-8, -7, -9},
     {-6, 1, 2},
-  }
+  },
+  ans
   );
+  for (int i : ans) cout << i " ";
   return 0;
 }
