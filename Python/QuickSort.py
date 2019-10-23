1eb33c6704c3ef93b19eaf4bf0caf8c970ca9623
shibin340 <eparis@atlassian.com>
2019-10-23 23:11:50 +0530

    Added Quick sort in python

---
+++ Python/QuickSort.py
@@ -0,0 +1,23 @@
+def quickSort(arr):
+    less = []
+    pivotList = []
+    more = []
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        for i in arr:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                more.append(i)
+            else:
+                pivotList.append(i)
+        less = quickSort(less)
+        more = quickSort(more)
+        return less + pivotList + more
+
+if __name__ == "__main__":
+    arr=[int(x) for x in input("Enter the array elements : ").split()]
+    a = quickSort(arr)
+    print(a)
