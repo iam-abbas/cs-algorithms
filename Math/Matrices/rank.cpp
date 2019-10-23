4f01c5d744a0dd76ebd69372b26ceacc47cbe410
mysterion <lunu1997@gmail.com>
2019-10-23 17:53:23 +0530

    added matrix rank algorithm

---
+++ Math/Matrices/rank.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+const double EPS = 1E-9;
+
+int compute_rank(vector<vector<double>> A) {
+    int n = A.size();
+    int m = A[0].size();
+
+    int rank = 0;
+    vector<bool> row_selected(n, false);
+    for (int i = 0; i < m; ++i) {
+        int j;
+        for (j = 0; j < n; ++j) {
+            if (!row_selected[j] && abs(A[j][i]) > EPS)
+                break;
+        }
+
+        if (j != n) {
+            ++rank;
+            row_selected[j] = true;
+            for (int p = i + 1; p < m; ++p)
+                A[j][p] /= A[j][i];
+            for (int k = 0; k < n; ++k) {
+                if (k != j && abs(A[k][i]) > EPS) {
+                    for (int p = i + 1; p < m; ++p)
+                        A[k][p] -= A[j][p] * A[k][i];
+                }
+            }
+        }
+    }
+    return rank;
+}
+
+int main() {
+  cout << compute_rank(
+  {
+    {-5, -6, 4},
+    {-8, -7, -9},
+    {-6, 1, 2},
+  }
+  );
+  return 0;
+}
