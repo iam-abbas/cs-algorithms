44bf566caa0c3e4bee173d66ea0e5810ed934c9e
Kurt Bachelder <kurt.bachelder@sierra-cedar.com>
2019-10-24 22:24:48 -0400

    largest AND smallest

--- Python/kth_element.py
+++ Python/kth_element.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 
-def kth_element(k,mylist):
+# Usage:  kth_element(place_of_element,largest_or_smallest,array)
+# If you are looking for the 3rd largest element of "myarray": kth_element(3,'largest',myarray)
+
+def kth_element(k,large_or_small,mylist):
+
+    if large_or_small == "largest":
+        modifier = 1
+    elif large_or_small == "smallest":
+        modifier = -1
+    else:
+        print("Valid values for the sort are 'largest' or 'smallest'")
+        exit(1)
+
     if len(mylist) < k or k < 1:
-        print("You've selected an index outside the bounds of the array.")
+        print("ERROR: Array size is " + str(len(mylist)) + ", but requested value is " + str(k) + "...")
         return None
 
     newlist = [None] * k
@@ -10,13 +22,14 @@ def kth_element(k,mylist):
     while len(mylist) > 0:
         test = mylist.pop()
         for i in range(0,k):
-            if test > newlist[i] or newlist[i] == None:
+            if newlist[i] == None or test * modifier > newlist[i] * modifier:
                 newlist.insert(i,test)
                 newlist.pop()
+                #print(newlist)  # Uncomment to see the value list build in action
                 break
 
     return(newlist[k-1])
 
 if __name__ == "__main__":
-    mylist = [ 42, 39, 857, 20, 3884, 82, 0, 84, 8, 3 ]
-    print kth_element(-2,mylist)
+    mylist = [ 42, 39, 857, 20, 3884, 82, 0, 84, 8, 3, -50, -1000 ]
+    print(kth_element(1,'smallest',mylist))
