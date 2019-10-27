2f1ed0d361db0ae2a7c7126e8866250dd02cacc6
jbhavya11 <57057239+jbhavya11@users.noreply.github.com>
2019-10-27 12:45:14 +0530

    euler totient in python code

---
+++ euler-totient-in_python.py
@@ -0,0 +1,29 @@
+# A simple Python3 program 
+# to calculate Euler's 
+# Totient Function 
+
+# Function to return 
+# gcd of a and b 
+def gcd(a, b): 
+
+	if (a == 0): 
+		return b 
+	return gcd(b % a, a) 
+
+# A simple method to evaluate 
+# Euler Totient Function 
+def phi(n): 
+
+	result = 1
+	for i in range(2, n): 
+		if (gcd(i, n) == 1): 
+			result+=1
+	return result 
+
+# Driver Code 
+for n in range(1, 11): 
+	print("phi(",n,") = ", 
+		phi(n), sep = "") 
+			
+# This code is contributed 
+# by Smitha 
